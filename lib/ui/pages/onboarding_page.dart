48109a41a61d258b817ec84692328955fd68f151
davadaviar <davadaviarsaputra2345@gmail.com>
2023-06-17 10:53:44 +0700

    Add carousel slider controller

--- lib/ui/pages/onboarding_page.dart
+++ lib/ui/pages/onboarding_page.dart
@@ -2,8 +2,29 @@ import 'package:bank_sha/shared/theme.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+
+  int currentIndex = 0;
+  CarouselController carouselController = CarouselController();
+
+  List<String> titles = [
+    'Grow Your \nFinancial Today',
+    'Build From \nZero to Freedom',
+    'Start Together'
+  ];
+
+  List<String> subtitles = [
+    'Our system is helping you to \nachieve a better goal',
+    'We provide tips for you so that \nyou can adapt easier',
+    'We will guide you to where \nyou wanted it too'
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +49,13 @@ class OnboardingPage extends StatelessWidget {
                 height: 331,
                 viewportFraction: 1,
                 enableInfiniteScroll: false,
-              )
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    currentIndex = index;
+                  });
+                },
+              ),
+              carouselController: carouselController,
             ),
              const SizedBox(
                 height: 80,
@@ -49,7 +76,7 @@ class OnboardingPage extends StatelessWidget {
               child: Column(
                 children: [
                   Text(
-                    'Grow Your \nFinancial Today',
+                    titles[currentIndex],
                     style: blackTextStyle.copyWith(
                       fontSize: 20,
                       fontWeight: semiBold,
@@ -60,7 +87,7 @@ class OnboardingPage extends StatelessWidget {
                     height: 26,
                   ),
                   Text(
-                    'Our system is helping you to \nachieve a better goal',
+                    subtitles[currentIndex],
                     style: greyTextStyle.copyWith(
                       fontSize: 16,
                     ),
@@ -76,7 +103,7 @@ class OnboardingPage extends StatelessWidget {
                         width: 12,
                         height: 12,
                         decoration: BoxDecoration(
-                          color: lightBlue,
+                          color: currentIndex == 0 ? lightBlue : lightGrey,
                           shape: BoxShape.circle,
                         ),
                       ),
@@ -85,7 +112,7 @@ class OnboardingPage extends StatelessWidget {
                         width: 12,
                         height: 12,
                         decoration: BoxDecoration(
-                          color: lightGrey,
+                          color: currentIndex == 1 ? lightBlue : lightGrey,
                           shape: BoxShape.circle,
                         ),
                       ),
@@ -94,7 +121,7 @@ class OnboardingPage extends StatelessWidget {
                         width: 12,
                         height: 12,
                         decoration: BoxDecoration(
-                          color: lightGrey,
+                          color: currentIndex == 2 ? lightBlue : lightGrey,
                           shape: BoxShape.circle,
                         ),
                       ),
@@ -116,7 +143,9 @@ class OnboardingPage extends StatelessWidget {
                           ]
                         ),
                         child: TextButton(
-                          onPressed: () {},
+                          onPressed: () {
+                            carouselController.nextPage();
+                          },
                           style: TextButton.styleFrom(
                             backgroundColor: primaryColor,
                             shape: RoundedRectangleBorder(
